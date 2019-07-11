285257b20f989880f5f4ec1fcc905f09c507355e
rqcao <rqcao@outlook.com>
2019-07-11 17:25:12 +0800

    :art:finish 4day

--- temp.js
+++ temp.js
@@ -1,10 +1,34 @@
-// 去除字符串中最后一个指定的字符
+// 写一个方法把下划线命名转成大驼峰命名
 
-function removeString(str,removeStr){
-  var index = str.lastIndexOf(removeStr);
-  var final = str.substring(0,index)+str.substring(index+1,str.length)
-  return final
+// 方法一
+function changeString(str) {
+  var temp = str.split('');
+  var flag = false;
+  var finalStr = '';
+  for (let i = 0; i < temp.length; i++) {
+    if (temp[i] === "_") {
+      flag = true;
+      continue;
+    } else {
+      if (flag === true) {
+        temp[i] = temp[i].toUpperCase();
+        flag = false;
+      }
+      finalStr += temp[i]
+    }
+  }
+  return finalStr
 }
-
-var result = removeString('qwetqwetqwe','t')
+var result = changeString('_______a_bc_d__')
 console.log(result)
+
+// 方法二
+function changeStr1(str) {
+  var final = str.split('_').reduce((total, curValue, curIndex, arr) => {
+    return total + curValue.substring(0, 1).toUpperCase() + curValue.substring(1);
+  })
+  return final;
+
+}
+var result1 = changeStr1('a_c_def__')
+console.log(result1)
