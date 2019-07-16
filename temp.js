9ec727b1ea02655d96adaaf4590e1a5fde21d72d
rqcao <rqcao@outlook.com>
2019-07-16 20:57:23 +0800

    :gift_heart:写一个获取当前url查询字符串中的参数的方法

--- temp.js
+++ temp.js
@@ -1,12 +1,32 @@
-// 简要描述下什么是回调函数并写一个例子出来
+// 写一个获取当前url查询字符串中的参数的方法
 
-function callback() {
-  alert("I am in the callback!");
+//方法1：
+function getUrlParam(str) {
+  var temp = str.split('?')[1]  //a=1&b=2&c=3#target
+  if(temp.indexOf('#')){
+    temp = temp.split('#')[0] //a=1&b=2&c=3
+  }
+  var final = temp.split('&').reduce((total,curValue,cueIndex,arr)=>{
+    var key = curValue.split('=')[0];
+    var value = curValue.split('=')[1];
+    total[key] = value;
+    return total;
+  },{})
+  return final;
 }
 
-function work(func) {
-  alert("I am calling the callback!");
-  func(); 
-}
+var a = "https://github.com/kk?tab=stars&assetId=311&page=DETAIL&projectPhase=2";
+var result = getUrlParam(a)
+console.log(result)
 
-work(callback);
+
+
+//方法2：
+function urlParam(str){
+  const param = {};
+  str.replace(/([^&=?]+)=([^&]+)/g,(m,$1,$2)=> param[$1] = $2);
+  return param;
+}
+var b = "https://github.com/kk?tab=stars&assetId=311&page=DETAIL&projectPhase=2";
+var result1 = urlParam(b)
+console.log(result1)
