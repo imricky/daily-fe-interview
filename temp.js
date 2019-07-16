87cda1d0479958d4f258533368bb235a995580e8
rqcao <rqcao@outlook.com>
2019-07-16 10:27:59 +0800

    :wolf:什么是回调函数

--- temp.js
+++ temp.js
@@ -1,10 +1,10 @@
-// 写一个判断数据类型的方法
-// /\[object\s|\]/g,
-function judgeDataType(data){
+// 简要描述下什么是回调函数并写一个例子出来
+
+function callbackFun(data){
   return Object.prototype.toString.call(data).replace(/\[object\s|\]/g,'')
 }
 
 var a = ['12','qw','12','qw','asd','qw','asd','asq']
 
-var result = judgeDataType(a);
+var result = callbackFun(a);
 console.log(result)
