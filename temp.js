ae08150490cfa37f5c7ab08bbb08dfbbd1e071d7
rqcao <rqcao@outlook.com>
2019-07-18 19:55:17 +0800

    :rice_scene:finish 16day  js实现返回顶部

--- temp.js
+++ temp.js
@@ -1,32 +1,7 @@
-// 写一个获取当前url查询字符串中的参数的方法
+// 返回到顶部的方法有哪些？把其中一个方法出来
 
 //方法1：
-function getUrlParam(str) {
-  var temp = str.split('?')[1]  //a=1&b=2&c=3#target
-  if(temp.indexOf('#')){
-    temp = temp.split('#')[0] //a=1&b=2&c=3
-  }
-  var final = temp.split('&').reduce((total,curValue,cueIndex,arr)=>{
-    var key = curValue.split('=')[0];
-    var value = curValue.split('=')[1];
-    total[key] = value;
-    return total;
-  },{})
-  return final;
+function backTop(){
+  window.scrollTo(0,0);
+  document.documentElement.scrollTop = 0;
 }
-
-var a = "https://github.com/kk?tab=stars&assetId=311&page=DETAIL&projectPhase=2";
-var result = getUrlParam(a)
-console.log(result)
-
-
-
-//方法2：
-function urlParam(str){
-  const param = {};
-  str.replace(/([^&=?]+)=([^&]+)/g,(m,$1,$2)=> param[$1] = $2);
-  return param;
-}
-var b = "https://github.com/kk?tab=stars&assetId=311&page=DETAIL&projectPhase=2";
-var result1 = urlParam(b)
-console.log(result1)
